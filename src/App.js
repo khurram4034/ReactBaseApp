b45849910f832ba0c2cbad9f73ef468e9c70492e
khurram4034 <khurram2201022@gmail.com>
2020-06-10 21:18:56 +0500

    App.js udated

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+      My First App Khurram Shehzad Ahmad
         </a>
       </header>
     </div>
